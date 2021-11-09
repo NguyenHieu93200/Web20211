5072b88d9d27ca17b66497ec0ad4d06da5451ce3
Nguyen Minh Hieu <hieu.nm932000@gmail.com>
2021-11-09 21:31:16 +0700

    update lab5

--- lab5/ex.php
+++ lab5/ex.php
@@ -1,31 +1,81 @@
 <?php
-    class Page{
-        private $page = '';
-        private $title = '';
-        private $year ;
-        private $copyright = '';
+class Page
+{
+    private string $page;
+    private string $title;
+    private int $year;
+    private const SPECIALC = array("&", "\"", "'", "<", ">");
+    private const REPLACEC = array("&amp;", "&quot;", "&#039;", "&lt;", "&gt;");
 
-        function __construct($title,$year,$copyright){
-            $this->title = $title;
-            $this->year = $year;
-            $this->copyright = $copyright;
-        }
-
-        private function addHeader() {
-            
-        }
-        public function addContent($content) {
-            $this->page = $content;
-        }
-        private function addFooter() {
-
-        }
-        public function get(){
-            return $this->page;
-        }
+    public function __construct()
+    {
+        $this->page = '';
+        $this->title = 'Auto create';
+        $this->year = date('Y');
     }
+    private function addHeader()
+    {
+        $this->page = "<header>" . $this->year . "</header>" . $this->page;
+    }
+    private function addFooter()
+    {
+        $this->page =   $this->page . "<footer>this it footer</footer>";
+    }
+    public function addContent(string $content)
+    {
+        $this->page = $this->page . $content;
+    }
+    public function get(): string
+    {
 
-    $page = new Page("Page1",2021,"Name");
-    $page->addContent("<br>This is a normal page.</br>");
-    print ($page->get());
+        $this->addHeader();
+        $this->addFooter();
+        $this->page = "<title>".$this->title."</title>".$this->page;
+        str_replace($this::SPECIALC, $this::REPLACEC, $this->page);
+        return $this->page;
+    }
+}
+
 ?>
+<header></header>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <input type="text" name="content" placeholder="input here">
+    <input type="submit" value="create" name="submit" />
+    <input type="submit" value="clear" name="clear" />
+</form>
+
+<?php
+if (isset($_POST["submit"])) {
+    $submitted = $_POST["submit"];
+    $content = $_POST["content"];
+    $file = fopen('data.txt', 'a');
+    fwrite($file, $content."\n");
+    fclose($file);
+} else if (isset($_POST["clear"])) {
+    $file = fopen('data.txt', 'w');
+    fclose($file);
+}
+
+$pages = array();
+if (file_exists('data.txt')) {
+    foreach (explode("\n", file_get_contents('data.txt')) as $content) {
+        if ($content == ''){
+            continue;
+        }
+        $page = new Page();
+        $page->addContent($content);
+        array_push($pages, $page);
+    }
+}
+
+foreach ($pages as $page) {
+    echo "<a onclick=\"newPage('" . $page->get() . "')\" href=#>Page</a>";
+}
+?>
+
+<script>
+    function newPage(page) {
+        var myWindow = window.open('', '_blank');
+        myWindow.document.write(page).text();
+    }
+</script>
